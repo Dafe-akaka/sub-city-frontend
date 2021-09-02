3bda174a4e1a29808712eab7c0e0296beaa74bd3
Dafe-akaka <dafe.akaka123@gmail.com>
2021-09-02 17:22:00 +0100

    prettier

--- src/components/Dashboard/AttendeeDashboard.tsx
+++ src/components/Dashboard/AttendeeDashboard.tsx
@@ -1,46 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
-
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 type EventParams = {
-    id: string;
-  };
+  id: string;
+};
 
 export interface Events {
-    Event_id: number;
-    organiser_name: string;
-    num_of_attendees: number;
-    total_cost: number;
-    description:string
+  Event_id: number;
+  organiser_name: string;
+  num_of_attendees: number;
+  total_cost: number;
+  description: string;
 }
 
 export default function AttendeeDashboard() {
-    let {id} =  useParams<EventParams>()
-    const [events,setEvents] = useState<Events[]>([])
+  let { id } = useParams<EventParams>();
+  const [events, setEvents] = useState<Events[]>([]);
 
-    const getEvent = async () => {
-        try {
-          const response = await fetch(`https://obscure-river-76343.herokuapp.com/event-info/${id}`);
-          const jsonData = await response.json();
-          setEvents(jsonData);
-        } catch (err) {
-          console.error(err.message);
-        }
-      };
-    
-      useEffect(() => {
-        getEvent();
-      },);
-    return (
-        <div>
-            {events.map((event) => (
-                <div key= {event.Event_id}>
-                    <p>{event.organiser_name}</p>
-                    <p>{event.description}</p>
-                    <p>{event.num_of_attendees}</p>
-                    <p>{event.total_cost}</p>
-                </div>
-            ))}
+  const getEvent = async () => {
+    try {
+      const response = await fetch(
+        `https://obscure-river-76343.herokuapp.com/event-info/${id}`
+      );
+      const jsonData = await response.json();
+      setEvents(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getEvent();
+  });
+  return (
+    <div>
+      {events.map((event) => (
+        <div key={event.Event_id}>
+          <p>{event.organiser_name}</p>
+          <p>{event.description}</p>
+          <p>{event.num_of_attendees}</p>
+          <p>{event.total_cost}</p>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
