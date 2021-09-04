c935814e349ec59d3e26aa61b7a42d57fb81c33b
Akwasi Appiah <akwasi.atta.appiah@gmail.com>
2021-09-04 12:35:02 +0100

    fixed infinte fetch and edited state to destructure object

--- src/components/Dashboard/AttendeeDashboard.tsx
+++ src/components/Dashboard/AttendeeDashboard.tsx
@@ -29,42 +29,45 @@ export interface Event {
 
 export default function AttendeeDashboard() {
   let { id } = useParams<EventParams>();
-  const [event, setEvent] = useState<Event>();
+  // const [event, setEvent] = useState<Event>();
   const [attendeeName, SetAttendeeName] = useState("");
-  //   const [cost, setCost] = useState("");
+  const [organiserName, setOrganiserName] = useState("");
+  const [dateOfEvent, setDateOfEvent] = useState("");
+  const [numOfAttendees, setnumOfAttendees] = useState(0);
+  const [totalCost, setTotalCost] = useState(0);
+  const [description, setDescription] = useState("");
+  const [timeOfEvent, setTimeOfEvent] = useState("");
 
-  // const getEvent = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       `https://obscure-river-76343.herokuapp.com/event-info/${id}`
-  //     );
-  //     const jsonData = await response.json();
-  //     setEvents(jsonData);
-  //   } catch (err) {
-  //     console.error(err.message);
-  //   }
-  // };
+  const costPerPerson = totalCost / numOfAttendees
+
+  const fetchLink = `https://obscure-river-76343.herokuapp.com/event-info/${id}`;
 
   useEffect(() => {
     const getEvent = async () => {
       try {
-        const fetchEventInfo = await fetch(
-          `https://obscure-river-76343.herokuapp.com/event-info/${id}`
-        );
-        const jsonData: Event = await fetchEventInfo.json();
-        setEvent(jsonData);
+        const fetchEventInfo = await fetch(fetchLink);
+        const jsonData: Event [] = await fetchEventInfo.json();
+        const eventInfo : Event = jsonData[0]
+
+        console.log({jsonData})
+        setOrganiserName(eventInfo.organiser_name);
+        setDateOfEvent(eventInfo.date_of_event);
+        setnumOfAttendees(eventInfo.num_of_attendees);
+        setTotalCost(eventInfo.total_cost);
+        setDescription(eventInfo.description);
+        setTimeOfEvent(eventInfo.time_of_event);
       } catch (err) {
         console.error(err.message);
       }
     };
 
     getEvent();
-  }, [event, id]);
+  }, [fetchLink]);
 
   const onSubmitAttendeeName = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault(); // we do not want it to refresh
     try {
-      const body = { attendeeName };
+      const body = { attendeeName, costPerPerson};
 
       const sendAttendeeInfo = await fetch(
         `http://obscure-river-76343.herokuapp.com/attendee/buy/${id}`,
@@ -81,7 +84,7 @@ export default function AttendeeDashboard() {
     }
   };
 
-  console.log({ event });
+  console.log({dateOfEvent});
 
   return (
     <div
@@ -94,26 +97,21 @@ export default function AttendeeDashboard() {
     >
       <Container boxShadow="dark-lg" p="10" rounded="md" bg="white" maxW="xl">
         {
-          <div key={event?.event_id}>
+          <div key={id}>
             <Box>
-              <Text fontSize="2xl">
-                Event Organisor: {event?.organiser_name}
-              </Text>
+              <Text fontSize="2xl">Event Organisor: {organiserName}</Text>
               <Text fontSize="2xl" mt="4">
                 Event Date:
-                {
-                  event?.date_of_event
-                  // .slice(0, 10)
-                }
+                {dateOfEvent?.slice(0, 10)}
               </Text>
               <Text fontSize="2xl" mt="4">
-                Event Time: {event?.time_of_event}
+                Event Time: {timeOfEvent}
               </Text>
               <Text fontSize="2xl" mt="4">
                 Event Description
               </Text>
               <Text fontSize="2xl" mt="2">
-                {event?.description}
+                {description}
               </Text>
               <HStack mt="4">
                 <Checkbox colorScheme="green" size="lg" isInvalid mr="20%">
@@ -121,17 +119,12 @@ export default function AttendeeDashboard() {
                 </Checkbox>
 
                 <Text fontSize="xl" ml="7">
-                  Cost Per Person: £
-                  {event !== undefined
-                    ? new Intl.NumberFormat("de-DE", {
-                        style: "currency",
-                        currency: "EUR",
-                      })
-                        .format(
-                          Number(event?.total_cost / event?.num_of_attendees)
-                        )
-                        .replace(/[^a-zA-Z0-9]/g, "")
-                    : null}
+                  Cost Per Person: 
+                  {new Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "GBP",
+                  })
+                    .format(Number(costPerPerson))}
                 </Text>
               </HStack>
             </Box>
